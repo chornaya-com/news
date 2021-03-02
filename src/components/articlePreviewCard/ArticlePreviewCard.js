ea8e62c91a958887d06b98632ac4f1e9b5f418c8
the-chornaya <me@chornaya.com>
2021-03-02 18:50:17 +0000

    add image to news preview card

--- src/components/articlePreviewCard/ArticlePreviewCard.js
+++ src/components/articlePreviewCard/ArticlePreviewCard.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import * as cn from './ArticlePreview.module.css';
+import * as cn from './ArticlePreviewCard.module.css';
 import {AppContext} from '../../App';
 
-export function ArticlePreview() {
+export function ArticlePreviewCard() {
     const {articles} = React.useContext(AppContext);
 
     return articles.map((article, i) => {
-        const date = new Date((Date.parse(article.publishedAt))).toDateString();
+        const date = new Date(article.publishedAt).toDateString();
         return (<div className={cn.articlePreviewCard} key={i}>
+            <div className={cn.imageContainer}><img className={cn.image} src={article.urlToImage} alt=""/></div>
             <div className={cn.articlePreviewTitle}>{article.title}</div>
             <div className={cn.articleSubtitle}>by {article.author} on {date}</div>
             <div>{article.description}</div>
--- src/components/mainPage/MainPage.js
+++ src/components/mainPage/MainPage.js
@@ -1,12 +1,12 @@
 import React from "react";
 import * as cn from "./MainPage.module.css";
-import {ArticlePreview} from '../articlePreview/ArticlePreview';
+import {ArticlePreviewCard} from '../articlePreviewCard/ArticlePreviewCard';
 
 export function MainPage() {
     return (
         <div className={cn.mainPage}>
             <div className={cn.mainPageHeader}>Latest News About Apple Inc.</div>
-            <div className={cn.newsFeed}><ArticlePreview/></div>
+            <div className={cn.newsFeed}><ArticlePreviewCard/></div>
         </div>
     )
 }
